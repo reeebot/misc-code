f6aeb551f7f22990ef789cd49d84248d43d1335f
Tim Meijome <reeebot@gmail.com>
2017-11-09 17:39:00 -0700

    removed unnecessary code

--- jeppesen programming assessment/q2.py
+++ jeppesen programming assessment/q2.py
@@ -6,45 +6,42 @@
 # (The exact data structure of the tree can be chosen freely.)
 
 
-# tree data
-# dataTree = {
-#             2: (
-#                 {
-#                     3: (
-#                         {
-#                             8
-#                         },
-#                         {
-#                             5
-#                         }
-#                     )
-#                 },
-#                 {
-#                     7
-#                 },
-#                 {
-#                     9: {
-#                         4: (
-#                         {
-#                             6
-#                         },
-#                         {
-#                             1
-#                         }
-#                     )
-#                     }
-#                 }
-#             )
-#         }
+#tree data
+dataTree = {
+            2: (
+                {
+                    3: (
+                        {
+                            8
+                        },
+                        {
+                            5
+                        }
+                    )
+                },
+                {
+                    7
+                },
+                {
+                    9: {
+                        4: (
+                        {
+                            6
+                        },
+                        {
+                            1
+                        }
+                    )
+                    }
+                }
+            )
+        }
 
 
-# import re
+import re
 
-def findLeaf(data):         # find nodes that don't have children
-    for x in data.items():
-        if isinstance(x, dict):
-            findLeaf(x)
-    else:
-        return data     # re.findall(r'\[([^]]*)\]',str(data))  # re to cleanup data
+def findLeaf(data):
+    for x in data.items():      # put nodes w no children into set[]
+        return re.findall(r'\[([^]]*)\]',str(data))  # re to cleanup data
       
-# print findLeaf(dataTree)
+print findLeaf(dataTree)
